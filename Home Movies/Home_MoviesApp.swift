d1cdd54b7ecbb2efa12bf3de6c6cc5050054e0c2
Brett Whitelaw <brett.whitelaw@me.com>
2022-12-17 01:56:33 -0500

    Initial Commit

---
+++ Home Movies/Home_MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_MoviesApp.swift
+//  Home Movies
+//
+//  Created by Brett Whitelaw on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Home_MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
